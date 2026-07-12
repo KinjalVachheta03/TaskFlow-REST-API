cf487319ff29a2c64c29e8ed4bbae435a4f7c48b
kinjal <vachhetakinjal1909@gmail.com>
2026-07-12 15:46:21 +0530

    Added Delete Task API

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -88,8 +88,96 @@ const getTaskById = async (req, res) => {
   }
 };
 
+
+// ================= Update Task =================
+const updateTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    // Check if task exists
+    if (!task) {
+      return res.status(404).json({
+        success: false,
+        message: "Task not found",
+      });
+    }
+
+    // Check ownership
+    if (task.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        success: false,
+        message: "Not authorized to update this task",
+      });
+    }
+
+    // Update task
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Task Updated Successfully",
+      task: updatedTask,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+// ================= Delete Task =================
+const deleteTask = async (req, res) => {
+  try {
+    // Find task
+    const task = await Task.findById(req.params.id);
+
+    // Check if task exists
+    if (!task) {
+      return res.status(404).json({
+        success: false,
+        message: "Task not found",
+      });
+    }
+
+    // Check ownership
+    if (task.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        success: false,
+        message: "Not authorized to delete this task",
+      });
+    }
+
+    // Delete task
+    await Task.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      message: "Task Deleted Successfully",
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
 module.exports = {
   createTask,
   getTasks,
   getTaskById,
+  updateTask,
+  deleteTask,
 }
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createTask , getTasks ,  getTaskById,} = require("../controllers/taskController");
+const { createTask , getTasks ,  getTaskById, updateTask, deleteTask,} = require("../controllers/taskController");
 const protect =  require("../middleware/authMiddleware");
 
 const router = express.Router();
@@ -7,4 +7,6 @@ const router = express.Router();
 router.post("/" , protect , createTask );
 router.get("/" , protect , getTasks);
 router.get("/:id", protect, getTaskById);
+router.put("/:id", protect, updateTask);
+router.delete("/:id", protect, deleteTask);
 module.exports = router;
